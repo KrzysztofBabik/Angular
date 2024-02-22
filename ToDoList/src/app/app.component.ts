25db8e0030f44b05f704c5745d925d2bab3ae2e9
KrzysztofBabik <krzysztofbabik.kb@gmail.com>
2024-02-22 15:18:51 +0100

    Implemented ToDoList

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -1,17 +1,33 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TasksListComponent } from './tasks-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  imports: [NgFor, TasksListComponent],
+  template: ` 
+    <h1 class="text-orange-500 bg-black py-4 text-xl text-center">
+      {{title}}
+    </h1> 
+    <app-tasks-list [tasks]="tasks" />
   `,
-  styles: [],
 })
 export class AppComponent {
-  title = 'ToDoList';
+  title = 'To Do List';
+  tasks = [
+    {
+      name: "Study",
+      done: false,
+    },
+    {
+      name: "Eat",
+      done: false,
+    },
+    {
+      name: "Sleep",
+      done: false,
+    }
+  ]
 }
